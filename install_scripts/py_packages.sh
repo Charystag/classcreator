cf50e7eaaabe3fd75b9a32f4f0e54068a09ec367
Charystag <57284562+Charystag@users.noreply.github.com>
2024-07-03 18:00:07 +0200

    feat(classcreator): :fire: Removed all code from Scripts directory :fire:

--- install_scripts/py_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip3 install numpy
-pip3 install pandas
-pip3 install virtualenv
-pip3 install scipy
-pip3 install matplotlib
-pip3 install scikit-learn
--- man_index.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-man_index_dir="man_index"
-mkdir -p "$man_index_dir"
-for i in {1..8}
-do apropos '.*' | grep -E ".*\($i\).*" 1>"$man_index_dir"/man"$i"_index
-done
